88c6827ebcecb0b4cfbf86dfad94dd58e06edcfc
Ben Kimball <bkimball97@gmail.com>
2021-04-12 09:35:37 -0500

    updated Trip's information

---
+++ scripts/database.js
@@ -0,0 +1,54 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+/* const database = {
+    fish: [
+        {
+            name: "Dan",
+            image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.21MKXtVWO4gAbmI3B4yw4gHaEK%26pid%3DApi&f=1",
+            species: "mahi mahi",
+            length: 20,
+            location: "Bahamas",
+            diet: "Fish diet",
+        },
+        {
+            name: "Paul",
+            image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.21MKXtVWO4gAbmI3B4yw4gHaEK%26pid%3DApi&f=1",
+            species: "mahi mahi",
+            length: 5,
+            location: "Bahamas",
+            diet: "Anything",
+        },
+        {
+            name: "Trip",
+            image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.21MKXtVWO4gAbmI3B4yw4gHaEK%26pid%3DApi&f=1",
+            species: "mahi mahi",
+            length: 20,
+            location: "Bahamas",
+            diet: "Fish diet",
+        },
+        {
+            name: "Dan",
+            image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.21MKXtVWO4gAbmI3B4yw4gHaEK%26pid%3DApi&f=1",
+            species: "mahi mahi",
+            length: 20,
+            location: "Bahamas",
+            diet: "Fish diet",
+        },
+        {
+            name: "Dan",
+            image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.21MKXtVWO4gAbmI3B4yw4gHaEK%26pid%3DApi&f=1",
+            species: "mahi mahi",
+            length: 20,
+            location: "Bahamas",
+            diet: "Fish food",
+        }
+    ]
+}
+
+export function fishCollection(){
+    return database
+} */
